5a58b4d5adffbdef3a94c39066119507fd08062e
David Baines <dave@lucolo.com>
2016-01-14 18:35:17 -0500

    Bugsnag

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,11 +6,7 @@ use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-// use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Bugsnag\BugsnagLaravel\BugsnagExceptionHandler as ExceptionHandler;
-
-// for SendinBlue
-use \Sendinblue\Mailin as Mailin;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
