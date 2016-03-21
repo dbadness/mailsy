71cff2c534d263881a24a2adad5d6665d5d6b003
David Baines <dave@lucolo.com>
2016-03-21 17:57:23 -0400

    Invoice email fixes

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -33,9 +33,9 @@ class APIController extends Controller
         // send confirmation email
         $subject = 'Mailsy Invoice - Paid';
         $body = 'Your Mailsy subscription has been successfully paid for. Thank you for using Mailsy!';
-        $body = '<br><br>Transaction ID: '.$transaction['id'];
-        $body = '<br>Date: '.date('m-d-Y',$stripe['created']);
-        $body = '<br>Amount: '.$amount.'<br><br>';
+        $body .= '<br><br>Transaction ID: '.$transaction['id'];
+        $body .= '<br>Date: '.date('m-d-Y',$stripe['created']);
+        $body .= '<br>Amount: '.$amount.'<br><br>';
 
         Utils::sendEmail($user->email,$subject,$body);
 
@@ -63,9 +63,9 @@ class APIController extends Controller
         // send confirmation email
         $subject = 'Mailsy Invoice - Declined';
         $body = 'There was a problem charging your credit card for your Mailsy subscription. Please log into Mailsy and use the Settings page to update your credit card.';
-        $body = '<br><br>Transaction ID: '.$transaction['id'];
-        $body = '<br>Date: '.date('m-d-Y',$stripe['created']);
-        $body = '<br>Amount: '.$amount.'<br><br>';
+        $body .= '<br><br>Transaction ID: '.$transaction['id'];
+        $body .= '<br>Date: '.date('m-d-Y',$stripe['created']);
+        $body .= '<br>Amount: '.$amount.'<br><br>';
 
         Utils::sendEmail($user->email,$subject,$body);
 
