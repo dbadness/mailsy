7f584d3c10f02a644bbded9c1637ff077ee6da79
David Baines <dave@lucolo.com>
2016-03-23 18:56:23 -0400

    Cancelation hot fix

--- resources/views/pages/confirm.blade.php
+++ resources/views/pages/confirm.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.master')
 
+@section('PageJS')
+	
+	
+
+@endsection
+
 @section('content')
 
 	<div class="jumbotron">
@@ -12,10 +18,30 @@
 			Since you're paying for other people, they'll also be allowed to use the paid version of Mailsy until {!! date('M-d-Y',$user->endDate) !!} after which they'll be downgraded to a free user account (10 emails per day).
 		@endif
 		<p>
-			<a class="btn btn-danger cancelButton" id='masterCancel' style='float:right;' role="button">Cancel Membership</a>
-			<a class="btn btn-primary" style='float:right;margin:0 20px 0 0;' href='/settings' role="button">Back to Settings</a>
+			{!! Form::token() !!}
+			<button class="btn btn-danger" id='masterCancelButton' style='float:right;' role="button">Cancel Membership</button>
+			<button class="btn btn-primary" style='float:right;margin:0 20px 0 0;' href='/settings' role="button">Back to Settings</button>
 		</p>
 		<div class="clear"></div>
 	</div>
+
+	<script>
+		// logic for cancelling the membership on the 'confirm' page
+		$('#masterCancelButton').on('click',function(){
+			$.ajax({
+				method : 'post',
+				url : '/membership/cancel',
+				data: {
+					'_token' : $('input[name=_token]').val()
+				},
+				success : function(){
+					window.location = '/settings?message=subscriptionCancelled';
+				},
+				error : function(){
+					alert('Something went wrong... please email hello@mailsy.co for help');
+				}
+			});
+		});
+	</script>
 
 @endsection
