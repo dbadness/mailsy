1d969582d63786b0b5746f99f0e5f558cbf2ecae
Andrew <andrewfribush@gmail.com>
2016-05-03 16:06:31 -0400

    another minor bug

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -197,7 +197,7 @@
 				    @foreach($children as $child)
 				    	<tr>
 				    		<td><p><a class="btn btn-primary" id='userModalButton' data-toggle="modal" data-target="#userModal{{$child->id}}">{!! $child->email !!}</a>
-				    		@if($user->team_admin == 1)
+				    		@if($child->team_admin == 1)
 					    		<a member='{!! $child->id !!}' class='btn btn-danger pull-right disabled'>You Cannot Downgrade Someone Leading a Team</a>
 					    	@else
 					    		<a member='{!! $child->id !!}' class='revokeAccessLink'>Downgrade to Free Account</a>
